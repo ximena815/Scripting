b5e12bdd24818a34adbf94c700470bd486e91a19
NattFdez <nataliafernandez1830@gmail.com>
2022-08-05 23:28:38 -0500

    Create PlayerController.cs

---
+++ First Game experiment/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 v3Force;
+
+    [SerializeField]
+    KeyCode keyPositive;
+
+    [SerializeField]
+    KeyCode keyNegative;
+
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+    }
+}
